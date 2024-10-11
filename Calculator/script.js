76f31a62103f923040e766b42ae737fc47cf526b
git config --global user.email you@example.com <developerintermix>
2024-10-11 16:13:58 +0200

    bezig met de color changing

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,25 @@
-console.log(1);
+// function
+// function getNumber() {
+//     let amount = document.getElementById("rate");
+//     console.log(amount)
+// }
+
+function getNumber() {
+    let amount = document.getElementById("amount").value;
+    console.log(amount)
+    let term = document.getElementById("term").value;
+    console.log(term);
+    let rate = document.getElementById("rate").value;
+    console.log(rate);  
+
+    if (amount <= 0){
+        let amount_color = document.getElementById("card__amount-color").style.color = "red"
+        console.log(amount_color)
+    };
+    // if (term == ""){
+        
+    // };
+    // if (rate == ""){
+        
+    // };
+}
