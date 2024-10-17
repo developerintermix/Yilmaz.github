b48c2b0e8f84fb389e38018207b8542b099d53a1
git config --global user.email you@example.com <developerintermix>
2024-10-17 13:53:30 +0200

    Icons toegevoegd.

--- Calculator/script.js
+++ Calculator/script.js
@@ -13,13 +13,27 @@ function getNumber() {
     console.log(rate);  
 
     if (amount <= 0){
-        let amount_color = document.getElementById("card__amount-color").style.color = "red"
-        console.log(amount_color)
-    };
-    // if (term == ""){
-        
-    // };
-    // if (rate == ""){
-        
-    // };
+        let amount_color = document.getElementById("card__amount-color").style.color = "red";
+        console.log(amount_color);
+    } else {
+        console.log("Succeed");
+    }
+
+    
+
+    // if (term <= 0){
+    //     let term_color = document.getElementById("card__term-color").style.color = "red";
+    //     console.log(term_color);
+    // } else {
+    //     console.log("Succeed");
+    // }
+
+    // if (rate <= 0){
+    //     let rate_color = document.getElementById("card__rate-color").style.color = "red";
+    //     console.log(rate_color);
+    // } else {
+    //     console.log("Succeed");
+    // }
+
+
 }
